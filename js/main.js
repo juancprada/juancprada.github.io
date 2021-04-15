4db9742429e313f011b2a5a57f358b46ff5e06e9
Juan Carlos Prada <jcp@juanprada.co>
2021-04-15 18:18:20 -0500

    Portfolio update

--- js/main.js
+++ js/main.js
@@ -1,50 +1,100 @@
-// Header animation functionality
-
-const headerTag = document.querySelector("header")
-var prevScrollpos = window.pageYOffset; //When the user scrolls down, hide the hider. When the user scrolls up, make it visible
-
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    headerTag.style.top = "0";
-  } else {
-    headerTag.style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
-// /Header animation functionality
-
 // Progress bar functionality
 
-const bodyTag = document.querySelector("body")
-const barTag = document.querySelector("div.progress-bar")
+  const bodyTag = document.querySelector("body")
+  const barTag = document.querySelector("div.progress-bar")
 
-document.addEventListener("scroll", function(){
-  const pixels = window.pageYOffset
-  const pageHeight = bodyTag.getBoundingClientRect().height
-  const totalScrollableDistance = pageHeight - window.innerHeight
+  document.addEventListener("scroll", function(){
+    const pixels = window.pageYOffset
+    const pageHeight = bodyTag.getBoundingClientRect().height
+    const totalScrollableDistance = pageHeight - window.innerHeight
 
-  const percentage = pixels / totalScrollableDistance
+    const percentage = pixels / totalScrollableDistance
 
-  barTag.style.width = `${100 * percentage}%`
+    barTag.style.width = `${100 * percentage}%`
 
-})
+  })
 
 // /Progress bar functionality
 
+// Gallery hover effect functionality
+
+  const canastaLink = document.querySelector("a.link-canasta")
+  const vibeLink = document.querySelector("a.link-vibe")
+  const amazonasLink = document.querySelector("a.link-amazonas")
+
+  const sunriseLink = document.querySelector("a.link-sunrise")
+  const avalancheLink = document.querySelector("a.link-avalanche")
+  const floatLink = document.querySelector("a.link-float")
+
+  const nbaLink = document.querySelector("a.link-nba")
+  const olympicLink = document.querySelector("a.link-olympic")
+  const kitkatLink = document.querySelector("a.link-kitkat")
+
+  const interfaceImages = document.querySelector("div.interface")
+  const illustrationImages = document.querySelector("div.illustration")
+  const onlinemediaImages = document.querySelector("div.online-media")
 
 
-// Mobile menu functionality
+  // While hovering, the corresponding image thumbnail appears
 
-const menuTag = document.querySelector("a.mobile-menu")
-const closeTag = document.querySelector("a.close-mobile-menu")
-const navTag = document.getElementById("mobile-nav")
-
-[menuTag, closeTag].forEach((tag)=>{
-  tag.addEventListener("click", function() {
-    navTag.classList.toggle("open")
+  canastaLink.addEventListener("mouseover", function() {
+    interfaceImages.innerHTML = `<img src="assets/images/thumbnail_canasta.png" alt="La Canasta &middot; Website Concept">`
   })
-})
 
-// /Mobile menu functionality
+  vibeLink.addEventListener("mouseover", function() {
+    interfaceImages.innerHTML = `<img src="assets/images/thumbnail_vibe.png" alt="Vibe Co-Living &middot; Web App Concept">`
+  })
+
+  amazonasLink.addEventListener("mouseover", function() {
+    interfaceImages.innerHTML = `<img src="assets/images/thumbnail_amazonas.png" alt="Destino Amazonas &middot; Landing Page Concept">`
+  })
+
+
+  sunriseLink.addEventListener("mouseover", function() {
+    illustrationImages.innerHTML = `<img src="assets/images/thumbnail_sunrise.png" alt="The Last Sunrise &middot; Digital Illustration">`
+  })
+
+  avalancheLink.addEventListener("mouseover", function() {
+    illustrationImages.innerHTML = `<img src="assets/images/thumbnail_avalanche.png" alt="Avalanche &middot; Digital Illustration">`
+  })
+
+  floatLink.addEventListener("mouseover", function() {
+    illustrationImages.innerHTML = `<img src="assets/images/thumbnail_float.png" alt="I Could Float Here Forever &middot; Digital Illustration">`
+  })
+
+
+  nbaLink.addEventListener("mouseover", function() {
+    onlinemediaImages.innerHTML = `<img src="assets/images/thumbnail_nba.png" alt="NBA League Pass &middot; Online Media + OOH Banners">`
+  })
+
+  olympicLink.addEventListener("mouseover", function() {
+    onlinemediaImages.innerHTML = `<img src="assets/images/thumbnail_olympic.png" alt="Olympic Channel &middot; Online Media">`
+  })
+
+  kitkatLink.addEventListener("mouseover", function() {
+    onlinemediaImages.innerHTML = `<img src="assets/images/thumbnail_kitkat.png" alt="KitKat Colombia &middot; Online Media">`
+  })
+
+
+  // When hovering is not happening, no image is displayed
+
+  const interfaceLinks = [canastaLink, vibeLink, amazonasLink]
+  const illustrationLinks = [sunriseLink, avalancheLink, floatLink]
+  const onlinemediaLinks = [nbaLink, olympicLink, kitkatLink]
+
+  interfaceLinks.forEach(item => {item.addEventListener("mouseout", function() {
+    interfaceImages.innerHTML = ""
+    })
+  })
+
+  illustrationLinks.forEach(item => {item.addEventListener("mouseout", function() {
+    illustrationImages.innerHTML = ""
+    })
+  })
+
+  onlinemediaLinks.forEach(item => {item.addEventListener("mouseout", function() {
+    onlinemediaImages.innerHTML = ""
+    })
+  })
+
+// /Gallery hover effect functionality
