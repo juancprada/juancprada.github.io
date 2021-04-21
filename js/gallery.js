4ab38a6153610020ad25d412bada5f9e62f90330
Juan Carlos Prada <jcp@juanprada.co>
2021-04-21 16:54:03 -0500

    Spanish version of the site (+ some other updates)

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,3 @@
-// Progress bar functionality
-
-  const bodyTag = document.querySelector("body")
-  const barTag = document.querySelector("div.progress-bar")
-
-  document.addEventListener("scroll", function(){
-    const pixels = window.pageYOffset
-    const pageHeight = bodyTag.getBoundingClientRect().height
-    const totalScrollableDistance = pageHeight - window.innerHeight
-
-    const percentage = pixels / totalScrollableDistance
-
-    barTag.style.width = `${100 * percentage}%`
-
-  })
-
-// /Progress bar functionality
-
 // Gallery hover effect functionality
 
   const canastaLink = document.querySelector("a.link-canasta")
